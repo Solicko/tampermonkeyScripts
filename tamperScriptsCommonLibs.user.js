7a3fd5522c804def84759bb106f0251b789ca660
Assaf Sela <asela@fb.com>
2022-11-03 13:41:03 -0700

    hideAllByClassName

--- tamperScriptsCommonLibs.user.js
+++ tamperScriptsCommonLibs.user.js
@@ -3,3 +3,20 @@ function hide(elm) {
         elm.style.display="none";
     }
 }
+
+function hideAllIds(ids) {
+    ids.forEach(function (item, index) {
+        var elm;
+        if (elm = document.getElementById(item)) {
+            hide(elm);
+        }
+    });
+}
+
+function hideAllByClassName(classnames) {
+    classnames.forEach(function (classname, index) {
+	Array.from(document.getElementsByClassName(classname)).forEach(function (item, index2) {
+		hide(item);
+	}
+    });
+}
