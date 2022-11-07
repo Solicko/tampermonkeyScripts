e7c0da8f2077e3e6850a3fa6bd0350e2f3088f93
Assaf Sela <asela@fb.com>
2022-11-07 12:12:40 -0800

    include jquery attempt #1

--- tamperScriptsCommonLibs.user.js
+++ tamperScriptsCommonLibs.user.js
@@ -1,3 +1,7 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.1.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 function hide(elm) {
     if (elm) {
         elm.style.display="none";
